0f27085eaacd6ad23737a52306a72aa3ad2642ba
deathxxx34 <minecraft.scasasdr@gmail.com>
2019-10-15 20:44:17 +0200

    JoueurIA update[jouer() -> jouer(Plateau)], add[attr profondeur, constructeur -> add profondeur, setProfondeur, jouer, max, simulerCoup, maxScoreDes6, maxScoreDe2]

--- src/main/java/JoueurIA.java
+++ src/main/java/JoueurIA.java
@@ -1,11 +1,66 @@
+import java.util.ArrayList;
+
 public class JoueurIA extends Joueur {
+
+    private int profondeur;
 
     public JoueurIA(String pseudo) {
         super(pseudo);
+        profondeur = 10;
     }
 
-    public int jouer() {
-        return 0;
+    public void setProfondeur(int profondeur) {
+        this.profondeur = 2*profondeur;
+    }
+
+    public int jouer(Plateau plateau) {
+        int res = max(simulerCoup(plateau, 1), profondeur);
+        int j = 1;
+        for(int i = 2; i<6; i++) {
+            int resbis = max(simulerCoup(plateau, i), profondeur);
+            if(resbis>res)
+                j=i;
+        }
+        return j;
+    }
+
+    private int max(Plateau p, int generationRestante){
+        if(generationRestante == 0){
+            Plateau pl = new Plateau(p);
+            int res = pl.jouerCase(1);
+            for(int i = 0; i < 5; i++){
+                pl = new Plateau(p);
+                int resbis = pl.jouerCase(i);
+                if(resbis>res)
+                    res = resbis;
+            }
+            if(res==-1)
+                return -1000;
+            return res;
+        }
+        return maxScoreDes6(
+                max(simulerCoup(p, 1), generationRestante-1),
+                max(simulerCoup(p, 2), generationRestante-1),
+                max(simulerCoup(p, 3), generationRestante-1),
+                max(simulerCoup(p, 4), generationRestante-1),
+                max(simulerCoup(p, 5), generationRestante-1),
+                max(simulerCoup(p, 6), generationRestante-1));
+    }
+
+    private Plateau simulerCoup(Plateau p, int i){
+        Plateau plateau = new Plateau(p);
+        plateau.jouerCase(i);
+        return plateau;
+    }
+
+    private int maxScoreDes6(int i1,int i2,int i3,int i4,int i5,int i6){
+        return maxScoreDe2(i1, maxScoreDe2(i2 ,maxScoreDe2(i3 ,maxScoreDe2(i4 ,maxScoreDe2(i5, i6)))));
+    }
+
+    private int maxScoreDe2(int i, int j){
+        if(i>j)
+            return i;
+        return j;
     }
 
 }
