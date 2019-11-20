6cec163323ae46d411ff3189c19bc9fc910f3514
yassine-ahchach <yassine.ahchach@etu.umontpellier.fr>
2019-11-20 13:29:31 +0100

    debug ihmbis

--- src/main/java/IHMbis.java
+++ src/main/java/IHMbis.java
@@ -64,7 +64,7 @@ public class IHMbis extends JFrame implements ActionListener {
         String joueurquivajouer;
         int cote;
         int coteplateau;
-        if (l.getText().contains(j1.getPseudo() + " choisissez une case à jouer")){
+        if (l.getText().contains(j1.getPseudo())){
             joueurquiajoue = j1;
             joueurquivajouer = j2.getPseudo();
             cote = 0;
